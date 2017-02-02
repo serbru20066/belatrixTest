b4060fbb6f56d10e02e75ef4a77add830bc5d9e2
LTSSetup <brunitoz@gmail.com>
2017-02-02 09:35:37 -0500

    Initial Commit

---
+++ pruebaTecnicaBelatrix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pruebaTecnicaBelatrix
+//
+//  Created by LTSSetup on 2/2/17.
+//  Copyright © 2017 BrunoCardenasTorres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pruebaTecnicaBelatrix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pruebaTecnicaBelatrix
+//
+//  Created by LTSSetup on 2/2/17.
+//  Copyright © 2017 BrunoCardenasTorres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
